be1119f532132e9eb2261e530e96b4a8c588b5b3
Sravanti <sravi.sravanti73@gmail.com>
2025-12-19 15:36:53 +0530

    Initial Commit

---
+++ net-web/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  net-web
+//
+//  Created by Sravanti on 19/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ net-web/net_webApp.swift
@@ -0,0 +1,17 @@
+//
+//  net_webApp.swift
+//  net-web
+//
+//  Created by Sravanti on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct net_webApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
